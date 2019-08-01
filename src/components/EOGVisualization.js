3396d7ec4801842521d16fc4699be9ec22eb76f6
nishanth <nishanthpunyamurthy027@gmail.com>
2019-08-01 06:33:05 +0530

    Subscription added

--- src/components/EOGVisualization.js
+++ src/components/EOGVisualization.js
@@ -1,3 +1,40 @@
 import React from "react";
+import SwitchPanel from "./SwitchPanel";
+import {
+    Provider as UrqlProvider,
+    createClient,
+    Subscription,
+    defaultExchanges,
+    subscriptionExchange
+} from "urql";
+import { SubscriptionClient } from "subscriptions-transport-ws";
+import Subscriber from "./Subscriber";
 
-export default () => <div>EOG Visualization</div>
+
+const subscriptionClient = new SubscriptionClient("ws://react.eogresources.com/graphql",
+    {
+        reconnect: true,
+        timeout: 20000
+    }
+);
+
+const client = createClient({
+    exchanges: [
+        ...defaultExchanges,
+        subscriptionExchange({
+            forwardSubscription: operation => subscriptionClient.request(operation)
+        })
+    ]
+});
+
+const newMessages = `
+subscription {
+  newMeasurement {metric, at, value, unit}
+}
+`;
+export default () => <UrqlProvider value={client}>
+    <Subscription query={newMessages}>
+        {({ data }) => <Subscriber data={data} />}
+    </Subscription>
+    <SwitchPanel />
+</UrqlProvider>
